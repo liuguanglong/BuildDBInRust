8153c7e2d345fa089087abe4f99912f1e0c1a4fc
guanglong.liu <guanglong.liu@inossem.com>
2024-07-08 22:54:22 -0400

    Add Encode & Decode

--- src/btree/table/record.rs
+++ src/btree/table/record.rs
@@ -1,12 +1,29 @@
 use crate::btree::table::table::TableDef;
 use crate::btree::table::value::Value;
 use crate::btree::table::value::ValueType;
-
+use std::fmt;
 
 //Record Table Row
 pub struct Record<'a>{
     Vals: Vec<Value>,
     def: &'a TableDef,
+}
+
+impl<'a> fmt::Display for Record<'a> {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"Record Content:\n|");
+        for i in 0..self.def.Cols.len()
+        {
+            write!(f,"{}|", String::from_utf8(self.def.Cols[i].to_vec()).unwrap());
+        } 
+        
+        for i in 0..self.def.Cols.len()
+        {
+            write!(f,"{}|", self.Vals[i]);
+        } 
+        write!(f,"\n")
+    }
 }
 
 impl<'a> Record<'a> {
@@ -44,7 +61,7 @@ impl<'a> Record<'a> {
         }
     }
 
-    pub fn GetColumnIndex(&self, key: &[u8])-> Option<usize> {
+    fn GetColumnIndex(&self, key: &[u8])-> Option<usize> {
         for i in 0..self.def.Cols.len()
         {
             let cmp = crate::btree::util::compare_arrays(&self.def.Cols[i], key);
@@ -55,6 +72,97 @@ impl<'a> Record<'a> {
         }
         return None;
     }
+
+    // order-preserving encoding
+    pub fn encodeValues(&self, list:&mut Vec<u8>) {
+        let mut idx: usize = self.def.PKeys as usize + 1;
+        while idx < self.def.Cols.len() 
+        {
+            self.encodeVal(idx, list);
+            idx += 1;
+        }
+    }
+
+    pub fn decodeValues(&mut self, list:&Vec<u8>) {
+        let mut pos: usize = 0;
+        let mut idx: usize = self.def.PKeys as usize + 1;
+        while idx < self.def.Cols.len() 
+        {
+            pos = self.decodeVal(list, idx, pos);
+            idx += 1;
+        }
+    }
+
+    fn encodeVal(&self, idx: usize, list:&mut Vec<u8>) {
+
+        match &self.Vals[idx]
+         {
+            Value::INT8(v) => list.extend_from_slice(&v.to_le_bytes()),
+            Value::INT16(v) => list.extend_from_slice(&v.to_le_bytes()),
+            Value::INT32(v) => list.extend_from_slice(&v.to_le_bytes()),
+            Value::INT64(v) => list.extend_from_slice(&v.to_le_bytes()),
+            Value::BOOL(v) => {
+                if *v == true {
+                    list.extend_from_slice(&[1;1]);
+                } else {
+                    list.extend_from_slice(&[0;1]);
+                }
+            },
+            Value::BYTES(v) => {
+                crate::btree::util::escapeString(v, list);
+                //list.extend_from_slice(v);
+                list.push(0);
+            },
+        }
+    }
+
+    fn decodeVal(&mut self, val:&[u8], idx: usize, pos: usize) -> usize {
+        match (self.def.Types[idx]) {
+            ValueType::INT8 => {
+                self.Vals[idx] = Value::INT8(i8::from_le_bytes([val[pos];1]));
+                return pos + 1;
+            },
+            ValueType::INT16 => {
+                self.Vals[idx] = Value::INT16(i16::from_le_bytes( val[pos..pos+2].try_into().unwrap() ));
+                return pos + 2;
+            },
+            ValueType::INT32 => {
+                self.Vals[idx] = Value::INT32(i32::from_le_bytes( val[pos..pos+4].try_into().unwrap() ));
+                return pos + 4;
+            },
+            ValueType::INT64 => {
+                self.Vals[idx] = Value::INT64(i64::from_le_bytes( val[pos..pos+8].try_into().unwrap() ));
+                return pos + 8;
+            },
+            ValueType::BOOL => {
+                if val[pos] == 1 {
+                    self.Vals[idx] = Value::BOOL(true);
+                } else {
+                    self.Vals[idx] = Value::BOOL(false);
+                }
+                return pos + 1;
+            },            
+            ValueType::BYTES => {
+                let mut end = pos;
+                while val[end] != 0
+                {
+                    end += 1;
+                }   
+
+                match &mut self.Vals[idx]
+                {
+                    Value::BYTES(v) =>
+                    {
+                        let ret = crate::btree::util::deescapeString(val[pos..end].try_into().unwrap());
+                        v.clear();
+                        v.extend_from_slice(&ret);                        
+                    },
+                    Other => {}
+                }
+                return end + 1;
+            },
+        }
+    }
 }
 
 #[cfg(test)]
@@ -62,25 +170,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_set()
+    fn test_record_get_set()
     {
         let mut table = TableDef{
             Prefix:3,
             Name: "person".as_bytes().to_vec(),
             Types : vec![ValueType::BYTES, ValueType::BYTES,ValueType::BYTES, ValueType::INT16, ValueType::BOOL ] ,
             Cols : vec!["id".as_bytes().to_vec() , "name".as_bytes().to_vec(),"address".as_bytes().to_vec(),"age".as_bytes().to_vec(),"married".as_bytes().to_vec() ] ,
-            PKeys : 1,
+            PKeys : 0,
             Indexes : vec![vec!["address".as_bytes().to_vec() , "married".as_bytes().to_vec()],vec!["age".as_bytes().to_vec()]],
             IndexPrefixes : vec![],
         };
 
         let mut rc = Record::new(&table);
+        rc.Set("id".as_bytes(), Value::BYTES("20".as_bytes().to_vec()));
         rc.Set("name".as_bytes(), Value::BYTES("John Water".as_bytes().to_vec()));
+        rc.Set("address".as_bytes(), Value::BYTES("Pointe-Claire".as_bytes().to_vec()));
+        rc.Set("age".as_bytes(), Value::INT16(20));
+        rc.Set("married".as_bytes(), Value::BOOL(false));
 
         let ret = rc.Get("name".as_bytes());
         match  ret {
             Some(v) => println!("Name:{v}"),
             None=>{}
         }
+
+        let mut list:Vec<u8> = Vec::new();
+        rc.encodeValues(&mut list);
+
+        println!("Vals Result:{:?} \n", list);
+
+        let mut rc1 = Record::new(&table);
+        rc1.decodeValues(&list);
+
+        println!("After Decode:{}",rc1);
+
     }
+
 }
--- src/btree/util.rs
+++ src/btree/util.rs
@@ -18,3 +18,52 @@ pub fn compare_arrays(a: &[u8], b: &[u8]) ->i32 {
     }
     return 0;
 }
+
+pub fn deescapeString(content: &[u8]) -> Vec<u8> {
+    let mut list:Vec<u8> = Vec::new();
+    //println!("Before dedescapString: {}", content);
+    let mut idx: usize = 0;
+    while (idx < content.len() - 1) {
+        if content[idx] == 1 {
+            if content[idx + 1] == 1 {
+                list.push(0x00);
+                idx += 2;
+            } 
+            else if content[idx + 1] == 2 
+            {
+                list.push(0x01);
+                idx += 2;
+            } else 
+            {
+                list.push(content[idx]);
+                idx += 1;
+            }
+        } else 
+        {
+            list.push(content[idx]);
+            idx += 1;
+        }
+    }
+    list.push(content[idx]);
+
+    return list;
+}
+
+
+// Strings are encoded as nul terminated strings,
+// escape the nul byte so that strings contain no nul byte.
+pub fn escapeString(content: &[u8], list:&mut Vec<u8>) {
+    let mut idx: usize = 0;
+    while idx < content.len() 
+    {
+        if content[idx] <= 1 
+        {
+            list.push(0x01);
+            list.push(content[idx] + 1);
+        } else 
+        {
+            list.push(content[idx]);
+        }
+        idx += 1;
+    }
+}
